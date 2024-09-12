88026f165eafb4a049438b43832552aee5bb140e
MrEcE0046 <emil_edman@hotmail.com>
2024-09-12 06:35:16 +0200

    Gjort övnings uppgifter

--- Code-Along/import random as rnd.py
+++ Code-Along/import random as rnd.py
@@ -1,28 +1,26 @@
 import random as rnd 
-rnd_numb = rnd.randint(1, 101)
-# rnd_numb = 55
+# rnd_numb = rnd.randint(1, 101)
+rnd_numb = 55
 # guess = rnd.randint(1, 100)
 low = 1
 high = 100
 # guess = 0
-guesses = 1
+guesses = 0
 
 
 while True:
-    mid = (low + high) // 2
-    guess = mid
     print(f"Guess a number between 1 - 100: {guess}")
-
+    guess = (low + high) // 2
 
     if guess > rnd_numb:
         print(f"{guesses}: Guess too high, try again...\n")
         guesses = guesses + 1
-        high = mid - 1 
+        high = guess - 1 
         
     elif guess < rnd_numb:
         print(f"{guesses}: Guess too low, try again...\n")
         guesses = guesses + 1
-        low = mid + 1
+        low = guess + 1
     else:
         print(f"{guesses}: You won!")
         break
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # python-programmering-EMIL-EDMAN
-# Emils repo för egna antenckningar, CodeAlongs och inlämningar.
+# Mitt repo för egna antenckningar, CodeAlongs och inlämningar.
+# (╯°□°）╯︵┻━┻
 
 # Väl mött!
+# Emil Edman
