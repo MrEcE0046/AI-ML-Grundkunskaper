dac8e4b2b721ddc0ed96274c8257decaf0fc4013
MrEcE0046 <emil_edman@hotmail.com>
2024-10-14 11:38:11 +0200

    Finskrivit labb3, påbörjat vg upg

--- Labs/labb3/labb3.py
+++ Labs/labb3/labb3.py
@@ -1,68 +1,69 @@
 import numpy as np, matplotlib.pyplot as plt, csv
 
-data = []
-with open("unlabelled_data.csv") as f:
-    rows = f.readlines()
-    for row in rows:
-        data.append([float(i) for i in row.split(",")])
+
+def readfile():
+    """ Open file and append to list """
+    data = []
+    with open("unlabelled_data.csv") as f:
+        rows = f.readlines()
+        for row in rows:
+            data.append([float(i) for i in row.split(",")])
+    return data
 
 
-# x = 5
-# k = -1.2
-# m = 0
-# y = k*x+m # -6
-# print(y) 
+def categories(data, k, m):
+    """ Catagories data according to y=kx+m """
 
-k = -1.2
-m = 0
-x = [-5,-4,-3,-2,-1,0,1,2,3,4,5]
-y = list()
-for xval in x:
-  y.append(k*xval + m)
-
-def categories(y, data):
-    k = -1.2
-    m = 0
-    
     data_label = []
     for x, y in data:
         val = k*x+m
-        if y < val:
+        if y > val:
             data_label.append((x, y, 0))
-        elif y > val:
+        elif y < val:
             data_label.append((x, y, 1))
     return data_label
 
-       
-label_data = categories(y, data)
-print(label_data)
 
-zeroes = [i for i in label_data if i[2] ==  0] # Separerar 1/0 från stora listan, bara för att plotta
-ones = [i for i in label_data if i[2] ==  1]
+def write_file(label_data):
+    """" Write new file with labels """
 
-x_values = [item[0] for item in zeroes] # Separerar x och y för att slippa 0/1
-y_values = [item[1] for item in zeroes]
-
-x_value = [item[0] for item in ones] # Separerar x och y för att slippa 0/1
-y_value = [item[1] for item in ones]
-
-with open("labelled_data.csv", "w", newline="") as ld:
-    writer = csv.writer(ld)
-    writer.writerows(label_data)
+    with open("labelled_data.csv", "w", newline="") as ld:
+        writer = csv.writer(ld)
+        writer.writerows(label_data)
 
 
-#print(zeros)
-#print(ones)
-#print(label_data)
-#print(data)
-# print(len(label_data))
-# print(len(zeroes))
-# print(len(ones))
+def plott(label_data, k, m):
+    """ Plot a line and scatterplot """
 
-# plt.plot((-5, 6),(5, -6),"r")
-plt.plot(x,y,"r")
-plt.scatter(*zip(*data))
-plt.scatter(x_value, y_value, c="blue")
-plt.scatter(x_values, y_values, c= "green")
-plt.legend(("y=kx+m", "Label 1", "Label 0"))
-plt.show()
+    x = [-5,-4,-3,-2,-1,0,1,2,3,4,5]
+    y = list()
+    for xval in x:
+        y.append(k*xval + m)
+
+    zeroes = [i for i in label_data if i[2] ==  0] # Separerar 1/0 från stora listan, bara för att plotta
+    ones = [i for i in label_data if i[2] ==  1]
+
+    plt.plot(x,y,"r")
+    plt.scatter(np.array(ones)[:, 0], np.array(ones)[:, 1], c="blue")
+    plt.scatter(np.array(zeroes)[:, 0], np.array(zeroes)[:, 1], c= "green")
+    plt.title(f"{len(zeroes)} of label-0 | {len(ones)} of label-1")
+    plt.xlabel("X-axle")
+    plt.ylabel("Y-axle")
+    plt.legend((f"y= {k}x {m}", "Label 1", "Label 0"))
+    plt.grid()
+    plt.show()
+
+
+k = -1.5
+m = 0
+
+data = readfile()
+label_data = categories(data, k, m)
+write_file(label_data)
+plott(label_data, k, m)
+
+
+
+
+
+
