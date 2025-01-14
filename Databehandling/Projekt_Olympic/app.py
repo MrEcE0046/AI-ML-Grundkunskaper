ee1ca63db8459a7fe1e74440a10704e5f2b6fa09
MrEcE0046 <emil_edman@hotmail.com>
2025-01-14 09:51:54 +0100

    "Inlett statistiska metoder"

--- Databehandling/Projekt_Olympic/app.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("athlete_events.csv")
-
-""" Tabell på antal medaljer per individ i tyskland """
-ger_df = df[df["NOC"] == "GER"]
-ger_df1 = pd.concat([ger_df,pd.get_dummies(df["Medal"])],axis = 1)
-ger_indv_medals = ger_df1.groupby("NOC").sum()[["Gold","Silver","Bronze"]].sort_values("Gold",ascending=False).reset_index()
-
