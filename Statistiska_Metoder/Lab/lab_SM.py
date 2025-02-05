035719d2a137de26c5da321e1c8f483a59a183b5
MrEcE0046 <emil_edman@hotmail.com>
2025-02-05 12:42:02 +0100

    tillbaka efter sjukdom, färdigställer lab

--- Statistiska_Metoder/Lab/lab_SM.py
+++ Statistiska_Metoder/Lab/lab_SM.py
@@ -6,6 +6,8 @@ class LinearRegression:
     def __init__(self, X, Y):
         self.X = X
         self.Y = Y
+        alpha = 0.05
+        confidence_level = 0.95
 
     @property
     def b(self):
@@ -19,44 +21,56 @@ class LinearRegression:
     def d(self):
         return len(self.b)-1
     
+    @property
+    def confidence_level(self):
+        pass
+    
     def SSE(self):
         return np.sum(np.square(self.Y - (self.X @ self.b)))
     
     def SSR(self):
-        return np.sum(((self.X @ self.b) - np.mean(self.Y))**2)
+        return self.Syy() - self.SSE()
     
-    def SST(self):
-        return np.sum((self.Y - np.mean(self.Y))**2)
+    def Syy(self):
+        return (self.n*np.sum(np.square(self.Y)) - np.square(np.sum(self.Y)))/self.n
+        # return np.sum((self.Y - np.mean(self.Y))**2)
 
     def var(self):
         # A function or method to calculate the variance.
-        var = self.SSE() / (self.n-self.d-1)
-        return var
+        return self.SSE() / (self.n-self.d-1)
 
-    def std_dev(self):
+    def std_dev(self): # = S
         # A function or method to calculate the standard deviation.
-        # std_dev = np.std(self.Y)
-        std_dev = np.sqrt(self.var())
-        return std_dev
+        # return np.std(self.Y)
+        return np.sqrt(self.var())
 
     def significance_regression(self):
         # A function or method to calculate the significance of the regression.
-        S = np.sqrt(self.var())
-        sig_statistics = (self.SSR() / self.d)/S
+        sig_statistics = (self.SSR() / self.d)/self.std_dev() # Ska vara std_dev istället för var
         p_significance = stats.f.sf(sig_statistics, self.d, self.n-self.d-1)
         return p_significance
 
     def relevance(self):
         # A function or method that reports the relevance of the regression (R2).
-        R2 = self.SSR() / self.SST()
-        return R2
+        return self.SSR() / self.Syy()
+    
+    def significance(self):
+        c = np.linalg.pinv(self.X.T@self.X)*self.var()
+        p_value_list = []
+        for i in range(len(self.b)):
+            if i == 0:
+                continue
+            sig_statistic = self.b[i] / (self.std_dev()*np.sqrt(c[i,i]))
+            p_value = 2*min(stats.t.cdf(sig_statistic, self.n-self.d-1), stats.t.sf(sig_statistic, self.n-self.d-1))
+            p_value_list.append(p_value)
+        return p_value_list
 
+    def pearsonr(self):
+        X = self.X[:,1:]
+        return [(i, j, stats.pearsonr(X[:,i], X[:,j])[0]) for i in range(X.shape[1]) for j in range(i + 1, X.shape[1])]
 
-
-
-
-
-
+    def confidence_interval(self):
+        
 
 
 
